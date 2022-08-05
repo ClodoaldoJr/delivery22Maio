dd522760dca08c3c36b469e1eb7fab5d8dcff909
unknown <clodoaldo.junior@ymail.comm>
2022-08-04 22:46:33 -0300

    exercicio iniciado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert("ready")
+// for (let i = 0; i <= 50; i++ ) {
+//     console.log(i)
+// }
+
+// for(let i = 0; i < 11; i++){
+//     console.log("7 x " + i + " = " + 7*i);
+// }
+
+// var texto = "Junior";
+// console.log(texto.repeat(8))
